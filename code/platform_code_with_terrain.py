b3213ae40488ce43a6dc3e9ec5c5746636107989
anabellebr22 <brodskya@bu.edu>
2023-06-30 15:38:52 -0400

    platform visualization script with new terrain map

--- code/platform_code_with_terrain.py
+++ code/platform_code_with_terrain.py
@@ -59,12 +59,12 @@ def plot_navigation_with_map(file_name, lon_col='Longitude', lat_col='Latitude',
 
 
 #f = '/Users/anabellebrodsky/Desktop/NASA/ADMG/IMPACTS/P-3/1IMPACTS_MetNav_P3B_20220225_R0.ict' from impacts 
-f='/Users/anabellebrodsky/Desktop/NASA/ADMG/FIREX-AQ/DC-8/168FIREXAQ-METNAV5HZ_DC8_20190730_R1.ict' #from firex-aq
-
+f ='/Users/anabellebrodsky/Desktop/NASA/ADMG/FIREX-AQ/DC-8/168FIREXAQ-METNAV5HZ_DC8_20190730_R1.ict' #from firex-aq
+#f='/Users/anabellebrodsky/Desktop/NASA/ADMG/IMPACTS/ER-2/2IMPACTS_MetNav_ER2_20220225_R0.ict' #from impacts - ER-2 aircraft
 map_image_path = '/Users/anabellebrodsky/Desktop/NASA/ADMG/blueMarble.png'
 
 plot_navigation_with_map(f, lon_col='Longitude', lat_col='Latitude', alt_col='MSL_GPS_Altitude',
-                         alt_units='m',extent = [-125, -104, 40, 50], cmap='jet',
+                         alt_units='m', extent = [-125, -104, 40, 50], cmap='jet',
                          skip_rows=76, map_image_path=map_image_path)
                          #to visualize east coat extent = [-80.7, -67.5, 36.5, 45.4]
                         #to visualize california extent = [-124.5, -113.5, 32.5, 42]
